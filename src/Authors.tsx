ba49757eb240237912f38916fd7e522c640fd182
ovindu <ovindunambukara@gmail.com>
2020-11-11 19:02:46 +0530

    create and list authors

--- src/Authors.tsx
+++ src/Authors.tsx
@@ -4,8 +4,14 @@ import AuthorsList from "./components/authors/AuthorsList";
 import AddAuthor from "./components/authors/AddAuthor";
 import CreateAuthor from "./components/authors/CreateAuthor";
 import UpdateAuthor from "./components/authors/UpdateAuthor";
+import {IAuthor} from "./components/types/LibraryTypes";
 
-const Authors: React.FC = () => {
+type AuthorsProps={
+    authors:IAuthor[]
+    onAuthorAdd:(newAuthor:IAuthor)=>void
+}
+
+const Authors: React.FC<AuthorsProps> = (props) => {
     const [isVisible,setIsVisible] = useState<boolean>(false);
     const [isUpdatable,setIsUpdatable] = useState<boolean>(false);
 
@@ -22,11 +28,11 @@ const Authors: React.FC = () => {
         <React.Fragment>
             <Container className="books m-1 p-0 mt-0 pt-0 pl-1 pr-3" fluid>
                 <h4 className="sub-title pb-2">Authors</h4>
-               <label className='font-italic'>No authors listed here</label>
-                <AuthorsList setIsUpdatable={changeUpdatable}/>
+                {(props.authors.length == 0) && <label className='font-italic'>No authors listed here</label>}
+                {(props.authors.length != 0) && <AuthorsList setIsUpdatable={changeUpdatable} authors={props.authors}/>}
                 <AddAuthor changeVisibility={changeVisibility}/>
                 {isUpdatable && <UpdateAuthor  isUpdatable={changeUpdatable}/>}
-                {isVisible && <CreateAuthor onEditorClosed={changeVisibility} />}
+                {isVisible && <CreateAuthor onEditorClosed={changeVisibility} onAuthorAdd={props.onAuthorAdd} />}
             </Container>
         </React.Fragment>
     );
--- src/components/ReadingArea.tsx
+++ src/components/ReadingArea.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import {Col, Container, Row} from "react-bootstrap";
 import Authors from "../Authors";
 import Books from "../Books";
+import {IAuthor} from "./types/LibraryTypes";
 
 const ReadingArea: React.FC = () => {
+    const [authors,setAuthors]=useState<IAuthor[]>([]);
+    const handleOnAuthorCreated=(newAuthor:IAuthor)=>{
+        const allAuthors:IAuthor[]= authors ? authors.slice():[];
+        allAuthors.push(newAuthor);
+        setAuthors(allAuthors);
+    }
+
     return (
         <Container fluid className='reading-area'>
             <Row>
                 <Col className="ml-4 mr-3 pl-0 pr-0 mt-0 pt-0"><Books/></Col>
                 <Col className="ml-5 mr-3 pl-0 pr-0 mt-0 pt-0">
-                    <Authors/>
+                    <Authors authors={authors} onAuthorAdd={handleOnAuthorCreated}/>
                 </Col>
             </Row>
         </Container>
