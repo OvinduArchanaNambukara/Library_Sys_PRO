78d3fdceda8a3a61d7b3f57de3af44fa863ad282
ovindu <ovindunambukara@gmail.com>
2020-11-12 02:30:43 +0530

    author validation added

--- src/components/authors/CreateAuthor.tsx
+++ src/components/authors/CreateAuthor.tsx
@@ -9,14 +9,25 @@ type CreateAuthorProps = {
 
 const CreateAuthor: React.FC<CreateAuthorProps> = (props) => {
     const [authorName,setAuthorName] = useState<string | null>(null);
+    const [validated,setValidated] = useState<boolean>(false);
 
     const handleOnSubmit = (e:FormEvent) => {
         e.preventDefault();
-        //@ts-ignore
-        const newAuthor: IAuthor = {name:authorName};
-        props.onAuthorAdd(newAuthor);
-        setAuthorName(null);
-        props.onEditorClosed(false);
+        e.stopPropagation();
+        if(authorName !== null && authorName !== ''){
+            const newAuthor: IAuthor = {name: authorName};
+            props.onAuthorAdd(newAuthor);
+            setAuthorName(null);
+            props.onEditorClosed(false);
+            setValidated(false);
+        }else {
+            setValidated(true);
+        }
+    }
+
+    const onChangeCreateAuthorName = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setValidated(false);
+        setAuthorName(e.target.value);
     }
 
     return(
@@ -26,10 +37,11 @@ const CreateAuthor: React.FC<CreateAuthorProps> = (props) => {
                 <i className='feather icon-x-circle text-dark text-right float-right cursor-pointer mt-2' onClick={() => props.onEditorClosed(false)}/>
             </Col>
             <Col xs={12} className='pl-5'>
-                <Form onSubmit={handleOnSubmit}>
+                <Form onSubmit={handleOnSubmit} noValidate validated={validated}>
                     <Form.Group controlId="authorNameID">
                         <Form.Label>Name of author</Form.Label>
-                        <Form.Control  className="new-author-input" type="text" placeholder="" required onChange={(e:React.ChangeEvent<HTMLInputElement>) => setAuthorName(e.target.value)}/>
+                        <Form.Control  className="new-author-input" type="text" placeholder="" required onChange={onChangeCreateAuthorName}/>
+                        <Form.Control.Feedback type="invalid">Please fill the author name</Form.Control.Feedback>
                     </Form.Group>
                     <Button type="submit" size='sm'  className='float-right create-button'>Create</Button>
                 </Form>
