44eb6a2ab242672749c6eab5b5334977e63987e5
ovindu <ovindunambukara@gmail.com>
2020-11-12 02:30:43 +0530

    author validation added

--- src/components/authors/UpdateAuthor.tsx
+++ src/components/authors/UpdateAuthor.tsx
@@ -12,19 +12,26 @@ type UpdateAuthorProps = {
 
 const UpdateAuthor: React.FC<UpdateAuthorProps> = (props) => {
     const [editAuthorName,setEditAuthorName] = useState<string>(props.authorNo!=null?props.authors[props.authorNo].name:'');
+    const [validated,setValidated] = useState(false);
 
     const handleOnSubmit = (e:FormEvent)=>{
         e.preventDefault();
-        if(editAuthorName == null){
-            alert("cannot be null");
-        }else{
-            // @ts-ignore
+        e.stopPropagation();
+        if(editAuthorName !== null && editAuthorName !== ''){
             const updateAuthor: IAuthor = {name: editAuthorName};
             // @ts-ignore
             props.onAuthorUpdate(updateAuthor, props.authorNo);
             setEditAuthorName('');
+            setValidated(false);
+            props.after();
+        }else{
+            setValidated(true);
         }
-        props.after();
+    }
+
+    const onChangeUpdateAuthorName = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setEditAuthorName(e.target.value);
+        setValidated(false);
     }
 
     return(
@@ -34,10 +41,11 @@ const UpdateAuthor: React.FC<UpdateAuthorProps> = (props) => {
                 <i className='feather icon-x-circle text-dark text-right float-right cursor-pointer mt-2' onClick={() => props.after()}/>
             </Col>
             <Col xs={12} className='pl-5'>
-                <Form onSubmit={handleOnSubmit}>
+                <Form onSubmit={handleOnSubmit} noValidate validated={validated}>
                     <Form.Group controlId="authorNameID">
                         <Form.Label>Name of author</Form.Label>
-                        <Form.Control className="new-author-input" type="text" value={editAuthorName} onChange={(e:React.ChangeEvent<HTMLInputElement>) => setEditAuthorName(e.target.value)}/>
+                        <Form.Control className="new-author-input" type="text" value={editAuthorName} required onChange={onChangeUpdateAuthorName}/>
+                        <Form.Control.Feedback type="invalid">Please fill the author name</Form.Control.Feedback>
                     </Form.Group>
                     <Button type="submit" size='sm' className='float-right create-button'>Update</Button>
                 </Form>
