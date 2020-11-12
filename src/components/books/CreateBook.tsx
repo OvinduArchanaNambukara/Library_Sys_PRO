9418c6147795b422eea0ef644f17e650ddd7eb9c
ovindu <ovindunambukara@gmail.com>
2020-11-12 11:20:55 +0530

    add validation to books

--- src/components/books/CreateBook.tsx
+++ src/components/books/CreateBook.tsx
@@ -1,7 +1,6 @@
 import React, {FormEvent, useEffect, useState} from "react";
 import {Col, Button, Form} from "react-bootstrap"
 import Select, {ValueType} from 'react-select';
-import {selectorstyles} from '../../assets/stylesheets/selectorstyles';
 import {IAuthor, IBook} from "../types/LibraryTypes";
 
 type CreateBookProps={
@@ -16,18 +15,22 @@ const CreateBook:React.FC<CreateBookProps> = (props) =>{
         const [isbn, setISBN] = useState<string | null>(null);
         const [author, setAuthor] = useState<string | null>(null)
         const [selectedAuthor, setSelectedAuthor] = useState(null);
+        const [validated, setValidated] = useState(false);
+        const [selectorColor, setSelectorColor] = useState<string>('#989898');
 
         const onBookSubmit = (event: FormEvent) => {
             event.preventDefault();
             if (title == null || isbn == null || author == null || title == '' || isbn == '' || author == '') {
-               alert('error')
-            } else {
+                setValidated(true);
+                author == null || author == '' ? setSelectorColor('#f80046') : setSelectorColor('#989898');
+            }else{
                 // @ts-ignore
                 props.onBookAdd({title: title, isbn: isbn, author:{name:author.value}});
                 setTitle(null);
                 setISBN(null);
                 setAuthor(null);
                 setSelectedAuthor(null)
+                setValidated(false);
             }
         }
 
@@ -39,6 +42,31 @@ const CreateBook:React.FC<CreateBookProps> = (props) =>{
         const handleSelectAuthor = (selectAuthor: ValueType<any>) => {
             setSelectedAuthor(selectAuthor);
             setAuthor(selectAuthor);
+            onChangeValidation();
+        }
+
+        const onChangeTitle = (e:React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value)
+            onChangeValidation();
+        }
+
+        const onChangeISBN = (e:React.ChangeEvent<HTMLInputElement>) => {
+            setISBN(e.target.value);
+            onChangeValidation();
+
+        }
+
+        const selectorstyles = {
+            control: (provided: any, state: any) => ({
+                ...provided,
+                borderRadius: 0,
+                border: '2px solid'+ selectorColor
+            })
+        }
+
+        const onChangeValidation = () => {
+            setValidated(false);
+            setSelectorColor('#989898');
         }
 
         return(
@@ -51,17 +79,19 @@ const CreateBook:React.FC<CreateBookProps> = (props) =>{
                     <i className='feather icon-x-circle text-dark text-right' onClick={() => props.changeVisibility(false)}/>
                 </Col>
             </Form.Row>
-            <Form className="pl-5" onSubmit={onBookSubmit}>
+            <Form className="pl-5" onSubmit={onBookSubmit} noValidate validated={validated}>
                 <Form.Row>
                     <Form.Group controlId="titleSelectID"  className="form-bootstrap-area">
                         <Form.Label>Title of Book</Form.Label>
-                        <Form.Control required type="text" className="form-input" onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setTitle(e.target.value)}/>
+                        <Form.Control required type="text" className="form-input" onChange={onChangeTitle}/>
+                        <Form.Control.Feedback type="invalid">Please fill the book title</Form.Control.Feedback>
                     </Form.Group>
                 </Form.Row>
                 <Form.Row>
                     <Form.Group controlId="isbnSelectID"   className="form-bootstrap-area">
                         <Form.Label>ISBN</Form.Label>
-                        <Form.Control className="form-input"  required type="text" onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setISBN(e.target.value)}/>
+                        <Form.Control className="form-input"  required type="text" onChange={onChangeISBN}/>
+                        <Form.Control.Feedback type="invalid">Please fill the ISBN</Form.Control.Feedback>
                     </Form.Group>
                 </Form.Row>
                 <Form.Row>
@@ -74,6 +104,7 @@ const CreateBook:React.FC<CreateBookProps> = (props) =>{
                             isClearable
                             value={selectedAuthor}
                             onChange={handleSelectAuthor}
+                            required
                         />
                     </Form.Group>
                 </Form.Row>
