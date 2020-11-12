c990cf67f3c9d0e738a763abefcab92b1123b11d
ovindu <ovindunambukara@gmail.com>
2020-11-12 11:20:55 +0530

    add validation to books

--- src/components/books/Updatebook.tsx
+++ src/components/books/Updatebook.tsx
@@ -1,7 +1,6 @@
 import React, {FormEvent, useState} from "react";
 import {Col, Button, Form} from "react-bootstrap"
 import Select, {ValueType} from 'react-select';
-import {selectorstyles} from "../../assets/stylesheets/selectorstyles";
 import {IAuthor, IBook} from "../types/LibraryTypes";
 
 type UpdateBookProps = {
@@ -16,15 +15,21 @@ const UpdateBook:React.FC<UpdateBookProps> = (props) =>{
     const [isbn, setISBN] = useState<string>(props.selectedBook.isbn);
     const [author, setAuthor] = useState<string | null>(null)
     const [selectedAuthor, setSelectedAuthor] = useState({label:props.selectedBook.author.name,value:props.selectedBook.author.name});
+    const [validated, setValidated] = useState(false);
+    const [selectorColor, setSelectorColor] = useState<string>('#989898');
+
+
 
     const onBookSubmit = (event: FormEvent) => {
         event.preventDefault();
         if (title == null || isbn == null || author == null || title == '' || isbn == '' || author == '') {
-            alert('error')
+            setValidated(true);
+            author == null || author == '' ? setSelectorColor('#f80046') : setSelectorColor('#989898');
         } else {
             // @ts-ignore
             const book: IBook = {title: title, isbn: isbn, author:{name:author.value}};
             props.onBookUpdate(book);
+            setValidated(false);
         }
     }
 
@@ -36,6 +41,30 @@ const UpdateBook:React.FC<UpdateBookProps> = (props) =>{
     const handleSelectAuthor = (selectAuthor: ValueType<any>) => {
         setSelectedAuthor(selectAuthor);
         setAuthor(selectAuthor);
+        onChangeValidation();
+    }
+
+    const onChangeTitle = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value)
+        onChangeValidation();
+    }
+
+    const onChangeISBN = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setISBN(e.target.value);
+        onChangeValidation();
+    }
+
+    const selectorstyles = {
+        control: (provided: any, state: any) => ({
+            ...provided,
+            borderRadius: 0,
+            border: '2px solid'+ selectorColor
+        })
+    }
+
+    const onChangeValidation = () => {
+        setValidated(false);
+        setSelectorColor('#989898');
     }
 
     return(
@@ -48,19 +77,21 @@ const UpdateBook:React.FC<UpdateBookProps> = (props) =>{
                     <i className='feather icon-x-circle text-dark text-right' onClick={() => props.onEditorClose()}/>
                 </Col>
             </Form.Row>
-            <Form className="pl-5" onSubmit={onBookSubmit}>
+            <Form className="pl-5" onSubmit={onBookSubmit} noValidate validated={validated}>
                 <Form.Row>
                     <Form.Group controlId="titleSelectID"  className="form-bootstrap-area">
                         <Form.Label>Title of Book</Form.Label>
                         <Form.Control required type="text" value={title} className="form-input" placeholder=""
-                                      onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setTitle(e.target.value)}/>
+                                      onChange={onChangeTitle}/>
+                        <Form.Control.Feedback type="invalid">Please fill the book title</Form.Control.Feedback>
                     </Form.Group>
                 </Form.Row>
                 <Form.Row>
                     <Form.Group controlId="isbnSelectID" className="form-bootstrap-area">
                         <Form.Label>ISBN</Form.Label>
                         <Form.Control className="form-input" value={isbn} required type="text"
-                                      placeholder="" onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setISBN(e.target.value)}/>
+                                      placeholder="" onChange={onChangeISBN}/>
+                        <Form.Control.Feedback type="invalid">Please fill the ISBN</Form.Control.Feedback>
                     </Form.Group>
                 </Form.Row>
                 <Form.Row>
